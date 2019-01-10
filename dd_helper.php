7efea8ca1441cde8fa14e9c6be004a926ca8fe4c
circuitmamu <circuitmamu@mailinator.com>
2019-01-10 17:18:53 +0530

    Multiple Dump and Die
    Multiple Dump and Die for unlimited arguments inside the function

--- dd_helper.php
+++ dd_helper.php
@@ -1,8 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Multiple Dump and Die and shows all together.
+ * Use : dd($var1, $var2,.....)
+ */
 if (!function_exists('dd')) {
-    function dd($value)
+    function dd()
     {
+        $arrays = func_get_args();
+        $count = count($arrays);
         echo '<pre>';
-        print_r($value);die();
+        for ( $i = 0; $i < func_num_args(); $i++ ){
+            $line_break = ($i+1==$count)?'':'<hr/><br/>';
+            printf( "%s ".$line_break, print_r($arrays[$i], true) );
+        }
+        echo '</pre>';
+        die();
     }
 }
