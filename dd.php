76e7cba865a19d61671ed688fe4db237d1fb02ed
singharyan015 <singharyan015@gmail.com>
2019-01-10 12:41:22 +0530

    Create dd.php

---
+++ dd.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (!function_exists('dd')) {
+    function dd($value)
+    {
+        echo '<pre>';
+        print_r($value);die();
+    }
+}
